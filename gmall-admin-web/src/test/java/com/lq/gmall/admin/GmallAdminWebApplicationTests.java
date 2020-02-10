fce0faa136ab6d11deb047d481dc30d6b4226b46
86158 <abilifox@foxmail>
2020-02-10 15:23:40 +0800

    项目结构创建完成......

---
+++ gmall-admin-web/src/test/java/com/lq/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lq.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-cms/src/test/java/com/lq/gmall/cms/GmallCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lq.gmall.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
