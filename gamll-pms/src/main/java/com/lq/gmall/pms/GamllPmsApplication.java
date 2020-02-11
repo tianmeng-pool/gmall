c304c643d4b0a69f74bffcbf9cd76eaa07585231
86158 <abilifox@foxmail>
2020-02-11 17:18:39 +0800

    项目配置完成.....

--- gamll-pms/src/main/java/com/lq/gmall/pms/GamllPmsApplication.java
+++ gamll-pms/src/main/java/com/lq/gmall/pms/GamllPmsApplication.java
@@ -1,8 +1,22 @@
 package com.lq.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1.整合dubbo
+ * 2.整合mybatisplus
+ *
+ * logstash整合：
+ * 1.导入logback-logstash-encoderjar包
+ * 2.导入日志配置文件
+ * 在kibana里面建立好日志的索引,就可以可视化检索
+ */
+
+@EnableDubbo
+@MapperScan(basePackages = "com.lq.gmall.pms.mapper")
 @SpringBootApplication
 public class GamllPmsApplication {
 
--- gamll-pms/src/test/java/com/lq/gmall/pms/GamllPmsApplicationTests.java
+++ gamll-pms/src/test/java/com/lq/gmall/pms/GamllPmsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.lq.gmall.pms;
 
+import com.lq.gmall.pms.entity.Product;
+import com.lq.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GamllPmsApplicationTests {
 
+    @Autowired
+    private ProductService productService;
+
     @Test
     void contextLoads() {
+
+        Product product = productService.getById(1);
+        System.out.println(product);
+
     }
 
 }
