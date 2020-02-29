bcb675554d35614ced6fcc6b2b9f13e3fcc176d1
86158 <abilifox@foxmail>
2020-02-29 15:32:23 +0800

    检索服务完成

--- gmall-search/src/main/java/com/lq/gmall/search/GmallSearchApplication.java
+++ gmall-search/src/main/java/com/lq/gmall/search/GmallSearchApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 数组类型的测试，提交需要同名属性多几个值就行
+ * 如:localhost:8080/search?catelog=19&catelog=20&catelog=21
+ */
 @EnableDubbo
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GmallSearchApplication {
--- gmall-search/src/main/java/com/lq/gmall/search/service/impl/SearchProductServiceImpl.java
+++ gmall-search/src/main/java/com/lq/gmall/search/service/impl/SearchProductServiceImpl.java
@@ -1,8 +1,10 @@
 package com.lq.gmall.search.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.lq.gmall.constant.EsConstant;
 import com.lq.gmall.search.SearchProductService;
+import com.lq.gmall.to.es.EsProduct;
 import com.lq.gmall.vo.es.SearchParam;
 import com.lq.gmall.vo.es.SearchResponse;
 import com.lq.gmall.vo.es.SearchResponseAttrVo;
@@ -28,7 +30,9 @@ import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author tianmeng
@@ -92,22 +96,92 @@ public class SearchProductServiceImpl implements SearchProductService {
             brandNames.add(keyAsString);
         });
 
-        SearchResponseAttrVo attrVo = new SearchResponseAttrVo();
-        attrVo.setName("品牌");
-        attrVo.setValue(brandNames);
+        SearchResponseAttrVo brand_AttrVo = new SearchResponseAttrVo();
+        brand_AttrVo.setName("品牌");
+        brand_AttrVo.setValue(brandNames);
 
-        searchResponse.setBrand(attrVo);
+        searchResponse.setBrand(brand_AttrVo);
+        //====================品牌数据封装完成==========================
 
         //searchResponse.setCatelog();//设置分类
         //封装分类数据
+        TermsAggregation category_agg = aggregations.getTermsAggregation("category_agg");
+        List<String> cate_list = new ArrayList<>();
+        category_agg.getBuckets().forEach((cate_Bucket) -> {
 
+            Map<String,String> map = new HashMap<>();
+
+            String cateGoryName = cate_Bucket.getKeyAsString();
+
+            //获取category_agg下面的子聚合
+            TermsAggregation categoryId_agg = cate_Bucket.getTermsAggregation("categoryId_agg");
+            String cateGoryId = categoryId_agg.getBuckets().get(0).getKeyAsString();
+
+            //把id和name存入map
+            map.put("id",cateGoryId);
+            map.put("name",cateGoryName);
+
+            //把map转为json字符串传给前段，json字符串里面包含id和name
+            String s = JSON.toJSONString(map);
+
+            cate_list.add(s);
+        });
+        SearchResponseAttrVo cate_Attrvo = new SearchResponseAttrVo();
+        //setValue()里面有id和name，就不需要设置setProductId()的值
+        cate_Attrvo.setName("分类");
+        cate_Attrvo.setValue(cate_list);
+
+        searchResponse.setCatelog(cate_Attrvo);
+        //==================分类数据封装完成================================
 
         //设置总记录数
         searchResponse.setTotal(execute.getTotal());
+        //=================总记录数封装完成=========================
 
-//        searchResponse.setAttrs();//设置筛选属性
+        //searchResponse.setAttrs();//设置筛选属性
+        //获取总聚合下面的子聚合下面的terms聚合
+        TermsAggregation attrNames = aggregations.getChildrenAggregation("attr_agg")
+                .getTermsAggregation("attrName_agg");
+        List<SearchResponseAttrVo> attrVos = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        //遍历terms聚合下的所有数据
+        attrNames.getBuckets().forEach((att_bucket) -> {
+            SearchResponseAttrVo vo = new SearchResponseAttrVo();
+            String attr_Name = att_bucket.getKeyAsString();
+            vo.setName(attr_Name);
+            //获取该terms聚合下的两个字聚合
+            TermsAggregation attrId_agg = att_bucket.getTermsAggregation("attrId_agg");
+            vo.setProductAttributeId(Long.parseLong(attrId_agg.getBuckets().get(0).getKeyAsString()));
+            TermsAggregation attrValue_agg = att_bucket.getTermsAggregation("attrValue_agg");
+            List<TermsAggregation.Entry> buckets = attrValue_agg.getBuckets();
+            //遍历属性值的buckets，把buckets里面的数据封装到list中
+            buckets.forEach((valueBucket) -> {
+                String attrValue = valueBucket.getKeyAsString();
+                list.add(attrValue);
+            });
+            //封装属性名对应的属性值
+            vo.setValue(list);
+            attrVos.add(vo);
+        });
 
-//        searchResponse.setProducts();//设置商品信息
+        searchResponse.setAttrs(attrVos);
+        //==================筛选属性封装完成=========================
+
+        //searchResponse.setProducts();//封装商品数据
+        List<SearchResult.Hit<EsProduct, Void>> hits = execute.getHits(EsProduct.class);
+        List<EsProduct> esProducts = new ArrayList<>();
+        hits.forEach((hit) -> {
+            EsProduct source = hit.source;
+            //提取高亮结果
+            String title = hit.highlight.get("skuProductInfos.skuTitle").get(0);
+            //设置高亮结果
+            source.setName(title);
+
+            esProducts.add(source);
+        });
+
+        searchResponse.setProducts(esProducts);
+        //================商品数据封装完成====================
 
         return searchResponse;
     }
